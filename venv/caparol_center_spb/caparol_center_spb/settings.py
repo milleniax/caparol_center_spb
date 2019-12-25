d3fe1fafabfaf950736dd76ff1e8afd9235e2e33
milleniax <marsel.abdullin.00@mail.ru>
2019-12-25 21:07:11 +0300

    Create static,media and templates folders

--- venv/caparol_center_spb/caparol_center_spb/settings.py
+++ venv/caparol_center_spb/caparol_center_spb/settings.py
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_DIRS = [
+    os.path.join(os.path.dirname(BASE_DIR,'media'))
+]

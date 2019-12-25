4ca27f128f8850972e5ef5b5b32d1286f6fcd4f0
milleniax <marsel.abdullin.00@mail.ru>
2019-12-25 16:26:52 +0300

    CreateProject

---
+++ venv/caparol_center_spb/caparol_center_spb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for caparol_center_spb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caparol_center_spb.settings')
+
+application = get_wsgi_application()

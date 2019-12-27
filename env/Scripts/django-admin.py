339af8127f272ab009baaa5e0de89c93c36cd83f
milleniax <marsel.abdullin.00@mail.ru>
2019-12-27 18:38:35 +0300

    Working virtual enviroment

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\marsel\desktop\bussiness\projects\sites\caparol_center_spb_project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

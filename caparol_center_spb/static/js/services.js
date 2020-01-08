7f08921cc730e7c7e11b007ca980f907a7789f08
Yaroslav <Yaroslavnikolaev2000@gmail.com>
2020-01-08 16:28:05 +0300

    services(img - svg)

--- caparol_center_spb/static/js/services.js
+++ caparol_center_spb/static/js/services.js
@@ -7,17 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
         decisionCard3 = document.querySelector('#card_decision-3');
 
     imgCard1.addEventListener('click', () => {
-        decisionCard1.style.border = '2px solid green';
+        decisionCard1.style.border = '2px solid #15C91C';
         decisionCard2.style.border = 'none';
         decisionCard3.style.border = 'none';
     })
     imgCard2.addEventListener('click', () => {
-        decisionCard2.style.border = '2px solid green';
+        decisionCard2.style.border = '2px solid #15C91C';
         decisionCard1.style.border = 'none';
         decisionCard3.style.border = 'none';
     })
     imgCard3.addEventListener('click', () => {
-        decisionCard3.style.border = '2px solid green';
+        decisionCard3.style.border = '2px solid #15C91C';
         decisionCard2.style.border = 'none';
         decisionCard1.style.border = 'none';
 

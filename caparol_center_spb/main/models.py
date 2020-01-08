19698157636384e2029f41eb3af613ad4d7f5748
milleniax <marsel.abdullin.00@mail.ru>
2020-01-08 11:33:38 +0300

    Create valid form

--- caparol_center_spb/main/models.py
+++ caparol_center_spb/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    email = models.EmailField()
+    initial = models.CharField(max_length=50)
+    question = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.initial
--- caparol_center_spb/main/urls.py
+++ caparol_center_spb/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.main_render, name='main_render')
+    path('', views.main_render, name='main_render'),
+    path('question', views.question, name='question')
 ]
